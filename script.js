cd0ccfaf8109d8fecd8ebc131a72e86f7bfafd06
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-22 09:47:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let element = document.getElementsByTagName("strong");
+let elements = document.getElementsByTagName("strong");
 
 function highlight() {
-    for (let i = 0; i < element.length; i++) {
-        element[i].style.color = "rgb(0, 128, 0)";
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.color = "rgb(0, 128, 0)";
     }
 }
 
 function return_normal() {
-    for (let i = 0; i < element.length; i++) {
-        element[i].style.color = "black";
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.color = "black";
     }
 }
+
 
     
 
