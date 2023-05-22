29d7c5b22262c137257cdf43bac52f9d4e4142a7
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-22 09:37:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
+let el = document.getElementsByTagName("strong");
+
 function highlight() {
-    //Write your code here
-	let p1 =document.querySelector('p');
-    let a= p1.querySelector('a');
-    let green =document.querySelector('#green');
-    let strong= green.querySelectorAll('strong');
-   
-    a.addEventListener("mouseover",function(){
-        strong.forEach(element => {
-            element.style.color="rgb(0, 128, 0)";
-          });
-    });
-    
-    
+    for (let i = 0; i < el.length; i++) {
+        el[i].style.color = "green";
+    }
 }
+
 function return_normal() {
-    //Write your code here
-    let p1 =document.querySelector('p');
-    let a= p1.querySelector('a');
-    let green =document.querySelector('#green');
-    let strong= green.querySelectorAll('strong');
-   
-    a.addEventListener("mouseout",function(){
-           strong.forEach(element => {
-            element.style.color="rgb(0, 0, 0)";
-          });
-    });
+    for (let i = 0; i < el.length; i++) {
+        el[i].style.color = "black";
+    }
+}
 
     
-}
+
 
