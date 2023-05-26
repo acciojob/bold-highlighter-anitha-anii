739e7f828dde37a52e0286345fe547790126c12a
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-26 10:26:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-let elements = document.getElementsByTagName("strong");
 
 function highlight() {
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].style.color = "rgb(0, 128, 0)";
-    }
+    //Write your code here
+  var boldWords = document.querySelectorAll('strong')
+  boldWords.forEach(function(word) {
+    word.style.color = 'green';
+  });
 }
-
 function return_normal() {
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].style.color = "black";
-    }
+   var boldWords = document.querySelectorAll('strong')
+  boldWords.forEach(function(word) {
+    word.style.color = 'black';
+  });
 }
 
 
